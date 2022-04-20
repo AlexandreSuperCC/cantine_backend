5e61284ba9b3ef62b77fa01ebc259e8e4abc3cfe
AlexandreSuperCC <yuancaocc@163.com>
2022-04-20 21:59:43 +0200

    first commit

---
+++ src/main/java/fr/utbm/cantine/CantineApplication.java
@@ -0,0 +1,13 @@
+package fr.utbm.cantine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CantineApplication.class, args);
+    }
+
+}
