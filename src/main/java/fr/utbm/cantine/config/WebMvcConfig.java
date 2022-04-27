577f90d7264615c4826072ee4b21838555266ea2
AlexandreSuperCC <yuancaocc@163.com>
2022-04-27 16:18:11 +0200

    basic architecture for index page : supplement

---
+++ src/main/java/fr/utbm/cantine/config/WebMvcConfig.java
@@ -0,0 +1,36 @@
+package fr.utbm.cantine.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Type WebMvcConfig.java
+ * @Desc add customized component to MVC
+ * @author yuan.cao@utbm.fr
+ * @date 27/04/2022 16:15
+ * @version 1.0
+ */
+@Component
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+
+    }
+
+    public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域的路由
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOriginPatterns("*")
+                // 是否允许证书（cookies）
+                .allowCredentials(true)
+                // 设置允许的方法
+                .allowedMethods("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
+
+}
