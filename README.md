680ce7d69701efe4d6aab271926987b23a675f75
AlexandreSuperCC <yuancaocc@163.com>
2022-04-28 12:36:27 +0200

    basic architecture for index page : supplement

---
+++ README.md
@@ -0,0 +1,7 @@
+## cantine application
+
+### database
+in this project we use the database mysql, the script for construction is file/cantine.sql 
+
+### starting port
+8084
---
+++ file/cantine.sql
@@ -0,0 +1,16 @@
+create database mo_cantine;
+use mo_cantine;
+create table `menu` (
+	`id` int(11) NOT NULL,
+	`name` varchar(50) NOT NULL DEFAULT '',
+	`type` int(11) NOT NULL DEFAULT 3 COMMENT '0 entrée, 1 plat, 2 dessert, 3 autres',
+	`rate` varchar(10),
+	`content` text,
+	`amount` int(11),
+	`day` int(11),
+	`imgurl` varchar(100),
+	`ts` varchar(50),
+	`dr` int(11) NOT NULL DEFAULT 0 COMMENT '0 active, 1 inactive',
+	primary key(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
