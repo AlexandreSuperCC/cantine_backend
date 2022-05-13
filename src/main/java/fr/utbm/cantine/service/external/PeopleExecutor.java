59831b63651d4be90cba1ad40157a0c1ac6b89e2
PoissonSA <paopzzz@163.com>
2022-05-13 23:23:08 +0200

    update algorithm

--- src/main/java/fr/utbm/cantine/service/external/PeopleExecutor.java
+++ src/main/java/fr/utbm/cantine/service/external/PeopleExecutor.java
@@ -9,7 +9,11 @@ import fr.utbm.cantine.model.PlatCapturerDomain;
 import fr.utbm.cantine.utils.APIResponse;
 import fr.utbm.cantine.utils.security.JwtUtil;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class PeopleExecutor extends Executor <PeopleCapturerDomain> {
+
+    private static ConcurrentHashMap<Integer, PeopleCapturerDomain> peopleMap = new ConcurrentHashMap<>();
 
     private volatile static PeopleExecutor peopleExecutor;
 
@@ -32,6 +36,9 @@ public class PeopleExecutor extends Executor <PeopleCapturerDomain> {
                 ||curObject.getNumberOfPeople()==null
                 ||curObject.getCid()==null
                 ||curObject.getToken()==null
+                ||curObject.getTotalIn()==null
+                ||curObject.getTotalOut()==null
+                ||curObject.getCurTime()==null
         ){
             throw BusinessException.withErrorCode(ErrorConstant.Common.PARAM_IS_EMPTY);
         }
@@ -45,10 +52,29 @@ public class PeopleExecutor extends Executor <PeopleCapturerDomain> {
             throw BusinessException.withErrorCode(ErrorConstant.Security.JWT_UNAUTHORIZED);
         }
 
-        String cantineID = String.valueOf(curObject.getCid());
+        Integer cantineID = curObject.getCid();
+
+        if( peopleMap.containsKey(cantineID) ){
+            PeopleCapturerDomain oldObject=peopleMap.get(cantineID);
+            if(curObject.getTotalOut()-oldObject.getTotalOut()!=0) {
+                curObject.setWaitTime(
+                        (int) ((double) (curObject.getCurTime() - oldObject.getCurTime()) / (curObject.getTotalOut() - oldObject.getTotalOut()) / 1000 * curObject.getNumberOfPeople())
+                );
+            }
+            else {
+                curObject.setWaitTime(
+                        (int) ((double)oldObject.getWaitTime()/oldObject.getNumberOfPeople()*curObject.getNumberOfPeople())
+                );
+            }
+            peopleMap.put(cantineID, curObject);
+        }
+        else {
+            peopleMap.put(cantineID,curObject);
+        }
+
         log.info("The Number of people waiting in the canteen ["+cantineID+"] is : "+curObject.getNumberOfPeople()+" , you have to wait ["+curObject.getWaitTime()+"] second , now sending data to client...");
         String jsonString = JSON.toJSONString(APIResponse.success(curObject,"people"));
-        WebSocketServer.sendInfo(jsonString,cantineID);
+        WebSocketServer.sendInfo(jsonString, String.valueOf(cantineID));
         /*inform client code using WebSocket*/
 
         return "success";
